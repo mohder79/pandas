2971fb6219ddfac90b01f2ab9cc655afd1dc4726
mohder <mohder1379@gmail.com>
2022-07-11 16:02:22 +0200

    Create head and tail data.py

---
+++ more/head and tail data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+data = 'F:\python\Bitstamp_BTCUSD_d.csv'  # data file address
+Columns = ['date', 'open', 'high', 'low', 'close',
+           'Volume USD']  # column you want to pandas read
+
+df = pd.read_csv(data, sep=',', usecols=Columns, header=1)
+# sep= separator (open your data file with notpad to see separator)
+# usecols = your column
+# header = Row number(s) to use as the column names, and the start of the data
+
+# reverse data . in csv file data start 2022 t0 2014 , so last data is 2014 .
+df = df[::-1].copy()
+
+# ///////////////
+print(df.head(4))  # head data
+
+print(df.tail(4))  # tail data
